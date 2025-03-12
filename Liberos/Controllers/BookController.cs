f32622be4d7adc4899fd0eb385a148e95533113c
Lucas Hergessel <lucas.hergessel@gmail.com>
2025-03-11 22:26:12 -0300

    Adicionando código inicial

---
+++ Liberos/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Liberos.Models;
+using Liberos.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Liberos.Controllers;
+
+[ApiController]
+[Route("api/books")]
+public class BookController : ControllerBase
+{
+    private readonly BookService _bookService = new();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Book>> GetBooks()
+    {
+        return Ok(_bookService.GetAllBooks());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Book> GetBook(int id)
+    {
+        var book = _bookService.GetBookById(id);
+        return book is not null ? Ok(book) : NotFound("Livro não encontrado");
+    }
+}

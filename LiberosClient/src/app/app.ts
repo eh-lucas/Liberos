9b6e23a08a771d1d12cffa5a280a6dd4d366cb71
vitormtr <vitor.triches@gmail.com>
2025-09-08 12:20:32 -0300

    Criados diretórios para organização do projeto, componente home e requisição ao endpoint para teste

--- LiberosClient/src/app/app.ts
+++ LiberosClient/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule], // apenas o necessário
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('LiberosClient');
+
+  books: any[] = [];
+  loading = false;
+  error = '';
+
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.fetchBooks();
+  }
+
+  fetchBooks(): void {
+    this.loading = true;
+    this.http.get<any[]>('http://localhost:7170/Book/')
+      .subscribe({
+        next: data => {
+          this.books = data;
+          this.loading = false;
+        },
+        error: err => {
+          console.error(err);
+          this.error = 'Erro ao carregar livros';
+          this.loading = false;
+        }
+      });
+  }
 }
